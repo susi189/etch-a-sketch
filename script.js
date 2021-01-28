ae081fe4c32e2c6b9cfee60be226b70d4f9f24c7
susi189 <ssteinhoff82@gmail.com>
2021-01-28 17:42:18 +0100

    aded funtionality

---
+++ script.js
@@ -0,0 +1,37 @@
+const container = document.getElementById('container');
+
+const startBtn = document.getElementById('start');
+
+startBtn.addEventListener('click', () => {
+    if(container.hasChildNodes()){
+       container.innerHTML = ''
+    }
+    let gridSize = prompt('How many squares per side? NOTE: The grid is a square itself, just input one number between 1 and 100');
+    createGrid(gridSize);
+})
+
+const clearBtn = document.getElementById('clear');
+
+clearBtn.addEventListener('click', () => {
+    let allGridElem = document.querySelectorAll('#grid-element');
+    allGridElem.forEach((elem) => {
+        elem.style.setProperty('background-color', 'white')
+    });
+})
+
+
+
+function createGrid(gridSize) {
+  container.style.setProperty('--grid-gridSize', gridSize);
+  for (let i=0; i<gridSize; i++) {
+    for(let j=0; j<gridSize; j++){
+        let elem = document.createElement('div');
+        container.appendChild(elem).id = 'grid-element';
+        elem.addEventListener('mouseover', (event) => {
+        event.target.style.setProperty('background-color', 'black')
+        });
+    };
+  };
+};
+
+
