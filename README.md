50014f29a942fd28ab711d5505d3a1594c0e1eb2
susi189 <ssteinhoff82@gmail.com>
2021-10-26 13:47:08 -0400

    CFeels more like drawing now

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 **About this project**
 
-This is one more of my early projects. Here I learned more about
+This is one more of my early projects. This is a very simple version of a [Etch-A-Sketch tool](https://susi189.github.io/etch-a-sketch/index.html). To begin drawing click the "Start" button, choose the size of the board and start drawing.
+
+Here I learned more about
 
 - DOM manipulation with vanilla JavaScript
 - CSS Grid
--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-const container = document.getElementById('container');
-
-const startBtn = document.getElementById('start');
-
-startBtn.addEventListener('click', () => {
-    if(container.hasChildNodes()){
-       container.innerHTML = ''
-    }
-    let gridSize = prompt('How many squares per side? NOTE: The grid is a square itself, just input one number between 1 and 100');
-    createGrid(gridSize);
-})
-
-const clearBtn = document.getElementById('clear');
-
-clearBtn.addEventListener('click', () => {
-    let allGridElem = document.querySelectorAll('#grid-element');
-    allGridElem.forEach((elem) => {
-        elem.style.setProperty('background-color', 'white')
-    });
-})
-
+const container = document.getElementById("container");
+const startBtn = document.getElementById("start");
+const clearBtn = document.getElementById("clear");
 
 function createGrid(gridSize) {
-  container.style.setProperty('--grid-gridSize', gridSize);
-  for (let i=0; i<gridSize; i++) {
-    for(let j=0; j<gridSize; j++){
-        let elem = document.createElement('div');
-        container.appendChild(elem).id = 'grid-element';
-        elem.addEventListener('mouseover', (event) => {
-        event.target.style.setProperty('background-color', 'black')
-        });
-    };
-  };
-};
+  container.style.setProperty("--grid-gridSize", gridSize);
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      let elem = document.createElement("div");
+      container.appendChild(elem).id = "grid-element";
+    }
+  }
+}
 
+function changeFields(event) {
+  if (event.buttons === 0) {
+    container.removeEventListener("mousemove", changeFields);
+  } else {
+    event.target.style.setProperty("background-color", "black");
+  }
+}
 
+startBtn.addEventListener("click", () => {
+  if (container.hasChildNodes()) {
+    container.innerHTML = "";
+  }
+  let gridSize = prompt(
+    "How many squares per side? NOTE: The grid is a square itself, just input one number between 1 and 100"
+  );
+  createGrid(gridSize);
+});
+
+clearBtn.addEventListener("click", () => {
+  let allGridElem = document.querySelectorAll("#grid-element");
+  allGridElem.forEach((elem) => {
+    elem.style.setProperty("background-color", "white");
+  });
+});
+
+container.addEventListener("mousedown", (event) => {
+  container.addEventListener("mousemove", changeFields);
+});
